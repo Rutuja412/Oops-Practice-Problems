4942b1ff6a968427ae6831dcf1292831840f1513
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-27 15:36:22 +0530

    [Rutuja]-Adding Inheritance example

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Animal
+    {
+        public void sounds() 
+        {
+            Console.WriteLine("Animal sound.....");
+        }
+    }
+    public class Dog : Animal
+    {
+        public void bark() 
+        {
+            Console.WriteLine("Barking..."); 
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+            d1.sounds();
+            d1.bark();
+        }
+    }
+}
