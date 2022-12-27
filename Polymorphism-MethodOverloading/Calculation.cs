bdbfae8bdfd4f5c6099c22d63c956a7e3cc67adf
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-27 16:27:14 +0530

    [Rutuja]-Adding Method Overloading

---
+++ Polymorphism-MethodOverloading/Calculation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism_MethodOverloading
+{
+    public class Calculation
+    {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ Polymorphism-MethodOverloading/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Polymorphism_MethodOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine(Calculation.add(12, 14));
+                Console.WriteLine(Calculation.add(12, 14, 25));
+            }
+        }
+    }
+}
