e07f1ae4a41b0767498a9e9ea8ff279d2a8659c4
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-27 19:00:14 +0530

    [Rutuja]-Adding Method Overriding

---
+++ Polymorphism-MethodOveriding/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism_MethodOveriding
+{
+    public class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+        public class Dog : Animal //derived class have same method
+        {
+            public override void eat()
+            {
+                Console.WriteLine("Eating bread...");
+            }
+        }
+
+    }
+}
---
+++ Polymorphism-MethodOveriding/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static Polymorphism_MethodOveriding.Animal;
+
+namespace Polymorphism_MethodOveriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dog obj = new Dog();
+            obj.eat();
+        }
+    }
+}
